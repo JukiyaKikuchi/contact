447686ab77b0310f68567eeef7fbcb7def3f7c7a
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2022-11-28 15:32:00 +0900

    userモデルとregisterモデルアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :image
+  belongs_to :register
 
   validates :birthday, presence: true
   validates :image,    presence: true
