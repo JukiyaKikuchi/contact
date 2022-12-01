8c75cf6312ec62bd98f3a269656a8d10d351a980
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2022-12-01 16:05:09 +0900

    registerコントローラーcreateアクション定義及びストロングパラメーター設定

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -7,4 +7,18 @@ class RegistersController < ApplicationController
   def new
     @register = Register.new
   end
+
+  def create
+    @register = Register.new(register_params)
+    if @register.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def register_params
+    params.require(:register).permit(:title, :general_comment, :image)
+  end
 end
