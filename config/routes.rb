cfc0a9d50d4917c2a6ac51fde9f38271b738a5e3
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2022-12-06 12:41:49 +0900

    registerコントローラ　ルーティング設定(edit,update)追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'registers#index'
-  resources :registers, only: [:index, :new, :create, :show, :destroy]
+  resources :registers
 end
