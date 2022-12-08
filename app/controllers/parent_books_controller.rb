25f8d9dbafa4b7667eb701fa890d75ad5c6a74f3
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2022-12-08 21:45:00 +0900

    parent_booksコントローラに(show,edit,update,destroy)アクション定義

--- app/controllers/parent_books_controller.rb
+++ app/controllers/parent_books_controller.rb
@@ -1,5 +1,8 @@
 class ParentBooksController < ApplicationController
+  before_action :set_parent_book, only: [:edit, :show, :update, ]
+
   def index
+    @parent_books = ParentBook.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -15,10 +18,35 @@ class ParentBooksController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def destroy
+    parent_book = ParentBook.find(params[:id])
+    if parent_book.destroy
+      redirect_to parent_books_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @parent_book.update(parent_book_params)
+      redirect_to parent_bookss_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def parent_book_params
-    params.require(:parent_book).permit(:parent_name, :parent_comment, :temperature, :poop_id, :breakfast, :attendance_id, :greeted_time_id, :greeted_name, :tell_number).merge(user_id: current_user.id)
+    params.require(:parent_book).permit(:parent_name, :parent_comment, :temperature, :mood_id, :breakfast, :attendance_id, :greeted_time_id, :greeted_name, :tell_number).merge(user_id: current_user.id)
+  end
+
+  def set_parent_book
+    @parent_book = ParentBook.find(params[:id])
   end
 
 end
