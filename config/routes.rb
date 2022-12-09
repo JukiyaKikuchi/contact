cf759990463efb9019c7aa686a0e00dbbf463e61
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2022-12-09 12:00:26 +0900

    teacher_commentsネストルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'registers#index'
   resources :registers
-  resources :parent_books
+  resources :parent_books do
+    resources :teacher_comments
+  end
 end
