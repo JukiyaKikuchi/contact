42f7baf67026f68de271cf7c473e7d34523eadc6
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2022-12-15 10:20:15 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'registers#index'
-  resources :registers
+  resources :registers 
   resources :parent_books do
-    resources :teacher_comments
+    resources :teacher_comments 
   end
 end
