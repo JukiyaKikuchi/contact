2b230a9708f5adb53a8ffb5772cc0f6c8f4dfadf
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2022-12-25 15:19:20 +0900

    parent_book　factorybot編集

--- spec/factories/parent_books.rb
+++ spec/factories/parent_books.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :parent_book do
+    parent_name  {"テスト"}
+    parent_comment  {Faker::Lorem.sentence}
+    temperature  {"36.5度"}
+    breakfast  {"テスト"}
+    greeted_name  {"テスト"}
+    greeted_time_id  {2}
+    attendance_id  {2}
+    mood_id  {2}
+    tell_number  {0000000000}
+    register_id  {2}
+
     
+    association :user
   end
 end
