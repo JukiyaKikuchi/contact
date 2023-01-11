35d16ee7170bfc2cfb02e81107c4f72c489d7e24
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2023-01-11 13:54:34 +0900

    teacher_commentモデル単体テスト

--- spec/models/teacher_comment_spec.rb
+++ spec/models/teacher_comment_spec.rb
@@ -14,12 +14,24 @@ RSpec.describe TeacherComment, type: :model do
 
     context '先生からのコメントが投稿できない' do
       it '先生の名前が空だと投稿できない' do
+        @teacher_comment.teacher_name = ''
+        @teacher_comment.valid?
+        expect(@teacher_comment.errors.full_messages).to include("Teacher name can't be blank")
       end
       it '先生からのコメントが空だと投稿できない' do
+        @teacher_comment.teacher_comment = ''
+        @teacher_comment.valid?
+        expect(@teacher_comment.errors.full_messages).to include("Teacher comment can't be blank")
       end
       it 'userが紐付いていないと投稿できない' do
+        @teacher_comment.user = nil
+        @teacher_comment.valid?
+        expect(@teacher_comment.errors.full_messages).to include("User must exist")
       end
       it 'parent_bookが紐付いていないと投稿できない' do
+        @teacher_comment.parent_book = nil
+        @teacher_comment.valid?
+        expect(@teacher_comment.errors.full_messages).to include("Parent book must exist")
       end
     end
   end
