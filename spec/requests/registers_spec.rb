f06ae7f24d556f4ad4d44471df754c1706552524
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2023-01-13 22:21:53 +0900

    ragistersコントローラ確認事項洗い出し

--- spec/requests/registers_spec.rb
+++ spec/requests/registers_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
+describe RegistersController, type: :request do
 
-RSpec.describe "Registers", type: :request do
+  before do
+    @register = FactoryBot.create(:register)
+  end
+
   describe "GET /registers" do
-    it "works! (now write some real specs)" do
-      get registers_index_path
-      expect(response).to have_http_status(200)
+    it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿済みのタイトルが存在する' do
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿済みのコメントが存在する' do
+    end
+    it 'ndexアクションにリクエストするとレスポンスに投稿済みの画像が存在する' do
     end
   end
 end
