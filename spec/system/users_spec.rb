5f8ce48b554c4b12a8fe3d6fb2e83129fbfca5ee
JukiyaKikuchi <shuxiyejuchi@gmail.com>
2023-01-18 22:12:21 +0900

    user結合テストit洗い出し

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -6,8 +6,12 @@ RSpec.describe "ユーザー新規登録", type: :system do
   end
 
   context 'ユーザー新規登録ができるとき' do
+    it '保存されているユーザーの情報と合致すればログインできる' do
+    end
   end
   context 'ユーザー新規登録ができないとき' do
+    it '保存されているユーザーの情報と一致しない場合はログインできない' do
+    end
   end
   
 end
